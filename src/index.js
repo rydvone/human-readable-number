817b84383ce28995198bf23dc589651f8ca8bc40
rydvone <rydvone@gmail.com>
2022-01-08 21:11:31 +0300

    feat: almost done 2

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 module.exports = function toReadable (number) {
   let wordArray0 = [
-    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    ' nine'];
   let wordArrayException = [
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-     'seventeen', 'eighteen', 'nineteen'];
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+    'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   let wordArray1 = [
-    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-     'ninety'];
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+    ' eighty', ' ninety'];
   let wordArray2 = [];
-
   wordArray2[0] = '';
   for (let i = 1; i < 10; i++) {
-    wordArray2[i] = wordArray0[i] + 'hundred';
+    wordArray2[i] = wordArray0[i] + ' hundred';
   }
-  
   arrWord = [wordArray0, wordArray1, wordArray2];
 
-  let ordinalNum = [];
   let newWord = '';
-  let index;
- 
-   // find exception from 10 to 19
-  function caseException11 (n) {
-    return ((n % 100 > 9) && (n % 100 < 20)) ? true : false;
+
+  if (number == 0) {
+    return 'zero';
+  }
+
+  // find exception from 10 to 19
+  function caseException11(number) {
+    return ((number % 100 > 9) && (number % 100 < 20)) ? true : false;
   }
 
   let j = 0;
-  let a = n;
+  let a = number;
   while (a >= 1) {
-    newWord = arrWord[j][a % 10] + newWord;
+    if (caseException11(number) && j == 0) {
+      newWord = wordArrayException[(a % 100) - 10];
+      a = Math.floor(a / 100);
+      j = 1;
+    } else {
+      console.log(j);
+      newWord = arrWord[j][a % 10] + ' ' + newWord;
+      a = Math.floor(a / 10);
+    }
     j++;
-    a = Math.floor(a / 10);
+
   }
-
-
-  newWord = [n % 10] + newWord;
-  newWord = wordArray1[Math.floor(n / 100)] + wordArray4[0] + newWord;
-
-
-
-
-
-
+  return newWord.trim();
 }
