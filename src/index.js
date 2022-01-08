86c9a90afa5b2fa8ba9027f6c351756933f050ea
rydvone <rydvone@gmail.com>
2022-01-08 20:36:49 +0300

    feat: almost done

--- src/index.js
+++ src/index.js
@@ -1,88 +1,45 @@
 module.exports = function toReadable (number) {
-  let wordArray1 = [
-    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-     ten, eleven, twelve,
-     thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, 
-     twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety];
-  let wordArray2 = [
+  let wordArray0 = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let wordArrayException = [
     'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
      'seventeen', 'eighteen', 'nineteen'];
-  let wordArray3 = [
-    'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+  let wordArray1 = [
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
      'ninety'];
-  let wordArray4 = [
-    'hundred'];
-    
+  let wordArray2 = [];
+
+  wordArray2[0] = '';
+  for (let i = 1; i < 10; i++) {
+    wordArray2[i] = wordArray0[i] + 'hundred';
+  }
+  
+  arrWord = [wordArray0, wordArray1, wordArray2];
 
   let ordinalNum = [];
   let newWord = '';
   let index;
-
+ 
    // find exception from 10 to 19
   function caseException11 (n) {
     return ((n % 100 > 9) && (n % 100 < 20)) ? true : false;
   }
 
-  function numDecomposition(n, index) {
-    let a = n;
-    while (a >= 1) {
-      ordinalNum[index] = a % 10;
-      a = Math.floor(a / 10);
-      index++;
-    }
-
-  // // find 10, 20, ... 90
-  // function caseException10 (n) {
-  //   return ((n % 100 > 9) && (n % 10 == 0)) ? true : false;
-  // }
-
-  // function numDecomposition (n, index) {
-  //   let a = n;
-  //   while (a >= 1) {
-  //     ordinalNum[index] = a % 10;
-  //     a = Math.floor(a / 10);
-  //     index++;
-  //   }
-  // }
-  // //index - content numbers ordinal
-
-  let tempWord = '';
-  switch (index) {
-    case index > 1 :
-      tempWord = ' hundred';
-      break;
-    case index > 2 :
-      tempWord = ' thousand';
-      break;
-    default :
-      tempWord = '';
+  let j = 0;
+  let a = n;
+  while (a >= 1) {
+    newWord = arrWord[j][a % 10] + newWord;
+    j++;
+    a = Math.floor(a / 10);
   }
 
-  if (caseException11(n)) {
 
-    ordinalNum[0] = n % 100;
-    numDecomposition(Math.floor(n % 100), 1);
-  } else {
-    numDecomposition(n, 0);
-  }
+  newWord = [n % 10] + newWord;
+  newWord = wordArray1[Math.floor(n / 100)] + wordArray4[0] + newWord;
 
-  // let numFindEqualWord;
-  for (let i = 0; true ; i++) {
-    newWord = ordinalNum
 
-  }
 
-    switch (ordinalNum[i]) {
-      case 1 :
-        wordNum[i] = 'one';
-        break;
-      case 2 :
-        wordNum[i] = 'two';
-        break;          
-      case 3 :
-        wordNum[i] = 'three';
-        break;  
-    }
-        
-  
+
+
+
 }
